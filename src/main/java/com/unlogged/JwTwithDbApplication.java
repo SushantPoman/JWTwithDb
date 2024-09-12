c14a00e41282dd2c25261e1be92f77d798054629
SushantPoman <sushantpoman94@gmail.com>
2024-09-12 20:03:12 +0530

    initial commit

---
+++ src/main/java/com/unlogged/JwTwithDbApplication.java
@@ -0,0 +1,13 @@
+package com.unlogged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTwithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwTwithDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unlogged/JwTwithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlogged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTwithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
